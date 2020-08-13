e88121fb996337eed35fa20f1ee57dd55a9b7e0f
Reilly-codefreak <jroduory@gmail.com>
2020-08-13 13:19:18 +0300

    most changes

--- js/index.ts
+++ js/index.ts
@@ -1,25 +1,28 @@
 //MENU FUNCTIONALITY
-var menuBtn = document.getElementById("menu-trigger");
-var homeLink = document.getElementById("home");
-var serviceLink = document.getElementById("service");
-var productLink = document.getElementById("product");
-var contactLink = document.getElementById("contact");
+const menuBtn = document.getElementById("menu-trigger");
+const homeLink = document.getElementById("home");
+const serviceLink = document.getElementById("service");
+const productLink = document.getElementById("product");
+const contactLink = document.getElementById("contact");
+
 menuBtn.addEventListener("click", function menu() {
     document.getElementById("hidden-menu").classList.add("open-menu");
-});
+})
+
 homeLink.addEventListener("click", function homepage() {
-    window.location.href = "index.html";
+    window.location.href = "index.html"
     document.getElementById("hidden-menu").style.display = "none";
-});
+})
+
 serviceLink.addEventListener("click", function servicepage() {
-    window.location.href = "services.html";
+    window.location.href = "services.html"
     document.getElementById("hidden-menu").style.display = "none";
-});
+})
 productLink.addEventListener("click", function productpage() {
-    window.location.href = "products.html";
+    window.location.href = "products.html"
     document.getElementById("hidden-menu").style.display = "none";
-});
+})
 contactLink.addEventListener("click", function contactpage() {
-    window.location.href = "contact.html";
+    window.location.href = "contact.html"
     document.getElementById("hidden-menu").style.display = "none";
-});
+})
